ce19b096f85063fef1360d2bde3fccb0230b3613
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-02-14 18:56:16 +0300

    Add about section other styles

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// const elSiteButton = document.querySelectorAll(".circle-box");
+// const elSiteCardReceived = document.querySelectorAll(".directors-item-received");
+// const elSiteCardBack = document.querySelectorAll(".directors-item-back");
+// elSiteButton.forEach((element, index) => {
+//   element.addEventListener("click", () => {
+//     elSiteCardReceived[index].classList.toggle("directors-item-received-active");
+//     elSiteCardBack[index].classList.toggle("directors-item-back-active");
+//   })
+// });
