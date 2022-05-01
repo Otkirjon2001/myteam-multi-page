c368c91927cd35a87962f7d4062354f6a0730937
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-05-01 11:19:29 +0300

    Add home page BEM technology

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-// const elSiteButton = document.querySelectorAll(".circle-box");
-// const elSiteCardReceived = document.querySelectorAll(".directors-item-received");
-// const elSiteCardBack = document.querySelectorAll(".directors-item-back");
-// elSiteButton.forEach((element, index) => {
-//   element.addEventListener("click", () => {
-//     elSiteCardReceived[index].classList.toggle("directors-item-received-active");
-//     elSiteCardBack[index].classList.toggle("directors-item-back-active");
-//   })
-// });
+// const elDirectorsList = document.querySelector(".directors-list");
+// const elSiteCardReceived = document.querySelector(".directors-item-received");
+// const elSiteCardBack = document.querySelector(".directors-item-back");
+
+// elSiteCardReceived.addEventListener('click', function(evt){
+//   if(evt.target.matches('.circle-box')){
+//     evt.target.parentElement.classList.add('directors-item-received-active');
+//     evt.target.parentElement.classList.toggle('directors-item-received-active');
+//     elSiteCardReceived.classList.toggle('directors-item-received-active');
+//     elSiteCardBack.classList.toggle('directors-item-back-active');
+//   }
+// })
+// elSiteCardBack.addEventListener('click', function(evt){
+//   if(evt.target.matches('.circle-box')){
+//     evt.target.parentElement.classList.toggle('directors-item-back-active');
+//     elSiteCardReceived.classList.toggle('directors-item-received-active');
+//     elSiteCardBack.classList.toggle('directors-item-back-active');
+//   }
+// })
